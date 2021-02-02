8cc76befe01d8cdce33e2c62cc9116b37f34195d
Brian Cook <brybcook@gmail.com>
2021-02-02 16:56:17 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -14,10 +14,9 @@ def loop_message_n_times(message, n)
   end
 end
 
-def output_array(message, n)
+def output_array(array)
   counter=0
-  while counter <=n do
-    puts message(counter)
+  while counter array.length do
     counter += 1
   end
 end
